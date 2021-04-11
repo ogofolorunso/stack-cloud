b4b977bf407bbafc7039e8fdae8ce892afe46039
Ogo Folorunso <ogofolorunso@gmail.com>
2021-04-10 21:18:25 -0400

    new initial commit

---
+++ Stack-ec2.tf
@@ -0,0 +1,64 @@
+## CREATE EC2 INSTANCE
+## ADD SECURITY GROUP
+## ADD TAGS
+## ATTACH KEY-NAME
+## ATTACH BOOTSTRAP SCRIPT FOR WORDPRESS
+resource "aws_instance" "web" {
+    ami           = "ami-0742b4e673072066f"
+    instance_type = "t2.micro"
+    user_data = file("EFSbootstap.sh")
+    security_groups = [ "tf-security" ]
+    key_name = "MyFirstEC2"
+
+
+    tags = {
+    Name = "TF--EC2"
+    }
+}
+
+##CREATE SECURITY GROUP AND ADD DIFFERENT PORTS
+resource "aws_security_group" "web-sg" {
+    name = "tf-security"
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port  = 22
+        protocol  = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 2049
+        to_port     = 2049
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+##CREATING AN SSH KEY
+resource "aws_key_pair" "ssh_key" {
+    key_name   = "Ogo-TF-key"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
